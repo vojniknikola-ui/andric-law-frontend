ac2b080f19d6446f8e348f919117bca7bea7ac2e
vojniknikola-ui <vojniknikola@gmail.com>
2025-11-05 12:55:46 +0100

    🐛 Fix: Ukloni rewrites iz next.config

--- next.config.js
+++ next.config.js
@@ -16,14 +16,7 @@ const nextConfig = {
       },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_STRAPI_URL}/api/:path*`,
-      },
-    ]
-  },
+
 }
 
 module.exports = nextConfig
